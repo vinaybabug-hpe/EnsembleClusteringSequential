46e8282e3a6913176c91d0cbe08bb96386523afa
vinaybabug <vinay.babu.g@hotmail.com>
2017-11-07 14:15:09 -0500

    Initial commit

---
+++ rcluster_ensemble_create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+#call executable with .mat files as arguments
+#./cluster_ensemble_create dataset/testcase1/PST_agglo.mat dataset/testcase1/X_100x50.mat "Sample Description" dataset/testcase1/output_idxs.mat
+./cluster_ensemble_create dataset/testcase1/PST_all.mat dataset/testcase1/X_100x50.mat "Sample Description" dataset/testcase1/output_idxs.mat
+#./cluster_ensemble_create dataset/testcase1/PST_all.mat dataset/testcase1/X_200x50.mat "Sample Description" dataset/testcase1/output_idxs_200_50.mat
+#./cluster_ensemble_create dataset/testcase1/PST_all.mat dataset/testcase1/X_300x50.mat "Sample Description" dataset/testcase1/output_idxs_300_50.mat
+#./cluster_ensemble_create dataset/testcase1/PST_all.mat dataset/testcase1/X_1000x50.mat "Sample Description" dataset/testcase1/output_idxs_1000_50.mat
+
+#./cluster_ensemble_create dataset/testcase1/PST_all.mat dataset/testcase1/X_4096x100.mat "Sample Description" dataset/testcase1/output_idxs_4096_100.mat
+
+
